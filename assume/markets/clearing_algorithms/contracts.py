a8f17974383bdd06c1ac2627b2ca244dc6518db0
Haoyu Huang <h.huang@fz-juelich.de>
2026-04-15 11:47:15 +0200

    Mark out dated test in contracts.py

--- assume/markets/clearing_algorithms/contracts.py
+++ assume/markets/clearing_algorithms/contracts.py
@@ -710,76 +710,81 @@ available_contracts: dict[str, Callable] = {
 contract_needs_market = ["CFD", "MPFIX", "MPVAR"]
 
 
-if __name__ == "__main__":
-    from dateutil import rrule as rr
-    from dateutil.relativedelta import relativedelta as rd
-
-    from assume.common.utils import get_available_products
-
-    simple_dayahead_auction_config = MarketConfig(
-        "simple_dayahead_auction",
-        market_products=[MarketProduct(rd(hours=+1), 1, rd(hours=1))],
-        opening_hours=rr.rrule(
-            rr.HOURLY,
-            dtstart=datetime(2005, 6, 1),
-            cache=True,
-        ),
-        opening_duration=timedelta(hours=1),
-        amount_unit="MW",
-        amount_tick=0.1,
-        price_unit="€/MW",
-        market_mechanism="pay_as_clear",
-    )
-    mr: MarketRole = PayAsBidContractRole(simple_dayahead_auction_config)
-    next_opening = simple_dayahead_auction_config.opening_hours.after(datetime.now())
-    products = get_available_products(
-        simple_dayahead_auction_config.market_products, next_opening
-    )
-    assert len(products) == 1
-
-    print(products)
-    start = products[0][0]
-    end = products[0][1]
-    only_hours = products[0][2]
-
-    orderbook: Orderbook = [
-        {
-            "start_time": start,
-            "end_time": end,
-            "volume": 120,
-            "price": 120,
-            "agent_id": "gen1",
-            "only_hours": None,
-        },
-        {
-            "start_time": start,
-            "end_time": end,
-            "volume": 80,
-            "price": 58,
-            "agent_id": "gen1",
-            "only_hours": None,
-        },
-        {
-            "start_time": start,
-            "end_time": end,
-            "volume": 100,
-            "price": 53,
-            "agent_id": "gen1",
-            "only_hours": None,
-        },
-        {
-            "start_time": start,
-            "end_time": end,
-            "volume": -180,
-            "price": 70,
-            "agent_id": "dem1",
-            "only_hours": None,
-        },
-    ]
-
-    accepted, rejected, meta = mr.clear(orderbook, products)
-    import pandas as pd
-
-    print(pd.DataFrame.from_dict(rejected))
-    print(pd.DataFrame.from_dict(accepted))
-    print(meta)
+#Following Test Code is outdated
+# if __name__ == "__main__":
+#     from dateutil import rrule as rr
+#     from dateutil.relativedelta import relativedelta as rd
+#
+#     from assume.common.utils import get_available_products
+#
+#     simple_dayahead_auction_config = MarketConfig(
+#         market_id="simple_dayahead_auction",
+#         market_products=[MarketProduct(timedelta(hours=1), 1, timedelta(hours=1))],
+#         additional_fields=["node"],
+#         opening_hours=rr.rrule(
+#             rr.HOURLY,
+#             dtstart=datetime(2005, 6, 1),
+#             until=datetime(2005, 6, 2),
+#             cache=True,
+#         ),
+#         opening_duration=timedelta(hours=1),
+#         volume_unit="MW",
+#         volume_tick=0.1,
+#         price_unit="€/MW",
+#         market_mechanism="pay_as_clear",
+#     )
+#     mr: MarketRole = PayAsBidContractRole(simple_dayahead_auction_config)
+#     next_opening = simple_dayahead_auction_config.opening_hours.after(
+#         datetime(2005, 6, 1)
+#     )
+#     products = get_available_products(
+#         simple_dayahead_auction_config.market_products, next_opening
+#     )
+#     assert len(products) == 1
+#
+#     print(products)
+#     start = products[0][0]
+#     end = products[0][1]
+#     only_hours = products[0][2]
+#
+#     orderbook: Orderbook = [
+#         {
+#             "start_time": start,
+#             "end_time": end,
+#             "volume": 120,
+#             "price": 120,
+#             "agent_id": "gen1",
+#             "only_hours": None,
+#         },
+#         {
+#             "start_time": start,
+#             "end_time": end,
+#             "volume": 80,
+#             "price": 58,
+#             "agent_id": "gen1",
+#             "only_hours": None,
+#         },
+#         {
+#             "start_time": start,
+#             "end_time": end,
+#             "volume": 100,
+#             "price": 53,
+#             "agent_id": "gen1",
+#             "only_hours": None,
+#         },
+#         {
+#             "start_time": start,
+#             "end_time": end,
+#             "volume": -180,
+#             "price": 70,
+#             "agent_id": "dem1",
+#             "only_hours": None,
+#         },
+#     ]
+#
+#     accepted, rejected, meta = mr.clear(orderbook, products)
+#     import pandas as pd
+#
+#     print(pd.DataFrame.from_dict(rejected))
+#     print(pd.DataFrame.from_dict(accepted))
+#     print(meta)
